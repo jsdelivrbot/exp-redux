ba3f22044eebc2e62b4c12bbdaa51dbe902711a1
Guilherme Santos <guilherme.goncalvessantos@gmail.com>
2017-04-07 17:27:49 -0300

    creating actions

---
+++ src/containers/book_list.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
+
+class BookList extends Component {
+  renderList() {
+    return this.props.books.map((book) => {
+      return (
+        <li key={book.title} className="list-group-item">{book.title}</li>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <ul className="list-group col-sm-4">
+        {this.renderList()}
+      </ul>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  // whatever is returned here will show up as props on the BookList
+  return {
+    books: state.books
+  }
+}
+
+// anything returned from this will be a props on the BookList
+function mapDispatchToProps(dispatch) {
+  // whenever selecBook is called, the result is passed to all reducers
+  return bindActionCreators({ selectBook: selecteBook }, dispatch);
+}
+
+// promote BookList to container - it needs to know about the dispatch method, selectBook
+// make it available as a prop
+export default connect (mapStateToProps, mapDispatchToProps) (BookList);
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Harry Potter' },
+    { title: 'The Lord of the Rings' },
+    { title: 'A Song of Ice and Fire' },
+    { title: 'American Gods' },
+  ]
+}
